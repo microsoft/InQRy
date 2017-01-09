63c1013db0c07fe4fe4eb2755d2af0af29faddbe
Eric Hanko <v-erhank@microsoft.com>
2017-01-09 14:56:57 -0800

    Comparison hardware info script
    Written by Thom McKiernan. Another example of what could be used on the
    Windows side for collecting hardware info.

---
+++ hardware_info.ps1
@@ -0,0 +1,22 @@
+# Local System Information v3
+# Shows details of currently running PC
+# Thom McKiernan 11/09/2014
+
+$computerSystem = Get-CimInstance CIM_ComputerSystem
+$computerBIOS = Get-CimInstance CIM_BIOSElement
+$computerOS = Get-CimInstance CIM_OperatingSystem
+$computerCPU = Get-CimInstance CIM_Processor
+$computerHDD = Get-CimInstance Win32_LogicalDisk -Filter "DeviceID = 'C:'"
+Clear-Host
+
+Write-Host "System Information for: " $computerSystem.Name -BackgroundColor DarkCyan
+"Manufacturer: " + $computerSystem.Manufacturer
+"Model: " + $computerSystem.Model
+"Serial Number: " + $computerBIOS.SerialNumber
+"CPU: " + $computerCPU.Name
+"HDD Capacity: "  + "{0:N2}" -f ($computerHDD.Size/1GB) + "GB"
+"HDD Space: " + "{0:P2}" -f ($computerHDD.FreeSpace/$computerHDD.Size) + " Free (" + "{0:N2}" -f ($computerHDD.FreeSpace/1GB) + "GB)"
+"RAM: " + "{0:N2}" -f ($computerSystem.TotalPhysicalMemory/1GB) + "GB"
+"Operating System: " + $computerOS.caption + ", Service Pack: " + $computerOS.ServicePackMajorVersion
+"User logged In: " + $computerSystem.UserName
+"Last Reboot: " + $computerOS.LastBootUpTime
