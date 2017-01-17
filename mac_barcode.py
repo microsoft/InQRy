dcff70028b5b51729e21ea9015921ac147e8b255
Eric Hanko <v-erhank@microsoft.com>
2017-01-17 14:58:16 -0800

    finalized required code to import system_profiler module

--- mac_barcode.py
+++ mac_barcode.py
@@ -1,17 +1,9 @@
-# import os
-# import sys
-# import inspect
-# from system_profiler_report_data import SystemProfilerReportData
-
-# Import process takes place here...
-
-# cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(
-#     inspect.getfile(inspect.currentframe()))[0],
-#     "/Users/erichanko/Dropbox/Projects/AutoInfra/mbu/modules/Infrastructure/InfraTi/Client/Environment")))
-# if cmd_subfolder not in sys.path:
-#     sys.path.insert(0, cmd_subfolder)
-
+import os
+import sys
 from qrcode import QRCode
+
+sys.path.append(os.path.abspath('../../../../mbu/modules'))
+from Infrastructure.InfraTi.Client.Environment.system_profiler import SystemProfiler
 
 system_data = SystemProfiler().profile_for_data_type(
     ['SPHardwareDataType', 'SPStorageDataType'])
