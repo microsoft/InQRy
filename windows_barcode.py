82f5a15344bd7d4eb808706edd8a6b297bc3873e
Eric Hanko <v-erhank@microsoft.com>
2017-01-17 16:36:46 -0800

    add ability to obtain output as xml

--- windows_barcode.py
+++ windows_barcode.py
@@ -1,8 +1,11 @@
 import subprocess
 from xml.etree.ElementTree import parse
 
-processor = subprocess.call(
-    ["C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe",
-     "Get-CimInstance", "Win32_Processor"])
 
-print(processor)
+def xml_data(hardware_property):
+    subprocess.call(
+        ["C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe",
+         "Get-CimInstance", hardware_property, "|",
+         "ConvertTo-XML", "-As", "String"])
+
+xml_data("Win32_Processor")
