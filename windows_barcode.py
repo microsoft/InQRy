2b303a4b54e828ffaee1c77c12dc6485a2ea8196
Eric Hanko <v-erhank@microsoft.com>
2017-01-17 16:57:21 -0800

    remove testing code; add print to xml_data function

--- windows_barcode.py
+++ windows_barcode.py
@@ -3,11 +3,8 @@ from xml.etree.ElementTree import parse
 
 
 def xml_data(hardware_property):
-    subprocess.call(
+    output = subprocess.call(
         ["C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe",
          "Get-CimInstance", hardware_property, "|",
          "ConvertTo-XML", "-As", "String"])
-
-
-processor = xml_data("Win32_Processor")
-print(processor)
+    print(output)
