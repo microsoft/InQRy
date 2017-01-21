3703ae1895e7c1d7b60f9bb83dc06585b6324e7d
Eric Hanko <v-erhank@microsoft.com>
2017-01-20 17:42:06 -0800

    system_profiler.py first commit

---
+++ system_profiler.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath('../../../../mbu/modules'))
+from Infrastructure.InfraTi.Client.Environment.system_profiler import SystemProfiler
+
+
+datatypes = ['SPHardwareDataType', 'SPStorageDataType']
+
+
+def system_profiler(datatype):
+    for datatype in datatypes:
+        SystemProfiler().profile_for_data_type(datatype)
