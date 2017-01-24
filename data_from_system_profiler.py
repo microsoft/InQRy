83afb4378fd2316621351f219a5336747c3c9232
Eric Hanko <v-erhank@microsoft.com>
2017-01-23 17:52:07 -0800

    modify for testability

--- data_from_system_profiler.py
+++ data_from_system_profiler.py
@@ -5,9 +5,11 @@ sys.path.append(os.path.abspath('../../../../mbu/modules'))
 from Infrastructure.InfraTi.Client.Environment.system_profiler import SystemProfiler
 
 
-datatypes = ['SPHardwareDataType', 'SPStorageDataType']
-
-
-def data_from_system_profiler(datatype):
-    for datatype in datatypes:
-        SystemProfiler().profile_for_data_type(datatype)
+def get_system_info_from_datatype(type_of_data):
+    try:
+        data = SystemProfiler().profile_for_data_type(
+            ['SP' + type_of_data + 'DataType'])
+        print(data)
+        return data
+    except TypeError:
+        pass
