f1a14555cb888bea7b075322cf54b75be5baceec
Eric Hanko <v-erhank@microsoft.com>
2017-01-24 15:53:31 -0800

    change storage argument from SPStorageDataType to SPSerialATADataType

--- mac_barcode.py
+++ mac_barcode.py
@@ -1,42 +1,70 @@
-from data_from_system_profiler import get_system_info_from_datatype
+from data_from_system_profiler import system_info_for_datatype
 from qrcode import QRCode
 
-# datatypes = ['SPHardwareDataType'], ['SPStorageDataType']
 
-components = ['machine_model', 'serial_number', 'physical_memory', 'cpu_type',
-              'current_processor_speed', 'machine_name',
-              'com.apple.corestorage.lvg.name',
-              'com.apple.corestorage.lvg.size',
-              'com.apple.corestorage.lvg.freeSpace']
+system_categories = ['SerialATA', 'Hardware', 'Network']
 
-hardware_specs = get_system_info_from_datatype('Hardware')
-storage_specs = get_system_info_from_datatype('Storage')
-
-# hardware = system_data[0]['_items'][0]
-# storage = system_data[1]['_items'][0]['com.apple.corestorage.lvg']
-
-qr = QRCode()
+hardware_components = ['machine_model', 'serial_number', 'physical_memory',
+                       'cpu_type', 'current_processor_speed', 'machine_name']
 
 
-def multidata_barcode():
-    for component in components:
-        if hardware.get(component):
-            unique_component = hardware.get(component)
-        else:
-            unique_component = storage.get(component)
-        qr.add_data(unique_component)
-        qr.add_data('\x09')
+hardware_specs = system_info_for_datatype('Hardware')
+storage_specs = system_info_for_datatype('SerialATA')
+network_specs = system_info_for_datatype('Network')
 
-    img = qr.make_image()
-    img.show()
+hardware = hardware_specs[0]['_items'][0]
+storage_keys = storage_specs[0]['_items'][0]['_items'][0].keys()
+storage = storage_specs[0]['_items'][0]['_items'][0]['device_model']
 
 
-def error_code(reason):
-    print(reason)
+# qr = QRCode()
+#
+#
+# def generate_item_list(datatype):
+#     data = system_info_for_datatype(datatype)
+#     items = data[0]['_items'][0]
+#     print(type(items))
+#     return items
+#
+#
+# generate_item_list('Hardware')
 
 
-def master_scan():
-    qr.code.add_data(hardware.get(data_list[1]))
+# for system_category in system_categories:
+#     generate_item_list(system_category)
+
+# empty_dict = {}
+# for component in hardware_components:
+#     unique_hardware_values = generate_item_list('Hardware')
+#     empty_dict[component] = unique_hardware_values[component]
+#
+# for component in storage_components:
+#     unique_storage_values = generate_item_list('Storage')
+#     empty_dict[component] = unique_storage_values[component]
+
+
+# def multidata_barcode():
+#     for component in components:
+#         if hardware.get(component):
+#             unique_component = hardware.get(component)
+#         else:
+#             unique_component = storage.get(component)
+#         qr.add_data(unique_component)
+#         qr.add_data('\x09')
+#
+#     img = qr.make_image()
+#     img.show()
+
+
+# def error_code(reason):
+#     print(reason)
+
+
+# data_list = []
+
+
+# def master_scan():
+#     qr.code.add_data(hardware.get(data_list[1]))
 
 
 # GENERIC
