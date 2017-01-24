a532422d4a6c0028e94477028468e9caa6a2835a
Eric Hanko <v-erhank@microsoft.com>
2017-01-24 15:53:06 -0800

    add string concatenation to system_profiler argument

--- data_from_system_profiler.py
+++ data_from_system_profiler.py
@@ -1,15 +1,14 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath('../../../../mbu/modules'))
-from Infrastructure.InfraTi.Client.Environment.system_profiler import SystemProfiler
+sys.path.append(os.path.abspath('./mbu/modules/Infrastructure'))
+from InfraTi.Client.Environment.system_profiler import SystemProfiler
 
 
-def get_system_info_from_datatype(type_of_data):
+def system_info_for_datatype(type_of_data):
     try:
         data = SystemProfiler().profile_for_data_type(
             ['SP' + type_of_data + 'DataType'])
-        print(data)
         return data
     except TypeError:
         pass
