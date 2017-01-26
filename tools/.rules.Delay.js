25af2a5100e6f00408d6854570e1c688f50b39ef
Eric Hanko <v-erhank@microsoft.com>
2017-01-25 16:29:02 -0800

    add other rules files

---
+++ tools/.rules.Delay.js
@@ -0,0 +1,5 @@
+rules_onDecode = function(a)
+{
+	a.data = a.data.replace(/\x13/g, "\x01X\x1ean//n\x04");
+	return;
+}
---
+++ tools/.rules.Enter.js
@@ -0,0 +1,5 @@
+rules_onDecode = function(a)
+{
+	a.data = c.data.replace(/\x54/g, "\x01X\x1ean//,\x04");
+	return a;
+}
