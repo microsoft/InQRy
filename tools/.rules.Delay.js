85bb634614b317bdb27d1589a3011b162818227b
Eric Hanko <v-erhank@microsoft.com>
2017-01-25 16:37:42 -0800

    fix typos

--- tools/.rules.Delay.js
+++ tools/.rules.Delay.js
@@ -1,5 +1,5 @@
 rules_onDecode = function(a)
 {
 	a.data = a.data.replace(/\x13/g, "\x01X\x1ean//n\x04");
-	return;
+	return a;
 }
--- tools/.rules.Enter.js
+++ tools/.rules.Enter.js
@@ -1,5 +1,5 @@
 rules_onDecode = function(a)
 {
-	a.data = c.data.replace(/\x54/g, "\x01X\x1ean//,\x04");
+	a.data = a.data.replace(/\x54/g, "\x01X\x1ean//,\x04");
 	return a;
 }
