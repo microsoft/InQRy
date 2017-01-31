5b65b2d1f4919a7ceca7248cce2b1e145a0d7b54
Eric Hanko <v-erhank@microsoft.com>
2017-01-31 13:46:19 -0800

    utilize subprocess to obtain individual elements for system information

--- windows_barcode.py
+++ windows_barcode.py
@@ -1,6 +1,8 @@
 import subprocess
 from xml.etree.ElementTree import parse
 
+powershell = "C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe"
+
 
 def xml_data(hardware_property):
     output = subprocess.call(
@@ -9,3 +11,13 @@ def xml_data(hardware_property):
          "ConvertTo-XML", "-As", "String"])
     print(output)
     return output
+
+
+def get_ciminstance(args, component):
+    item = subprocess.check_output(
+        [powershell, "Get-CimInstance", args, "|", "Select-Object", component])
+    return item
+
+
+print(get_ciminstance("CIM_ComputerSystem", "Name"))
+print(get_ciminstance("CIM_BIOSElement", "SerialNumber"))
