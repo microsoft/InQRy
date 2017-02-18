8875fad731b665136908a2ce12be6921256ee60c
Eric Hanko <v-erhank@microsoft.com>
2017-02-17 18:18:19 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyBar',
+      version='0.1',
+      description='Generate QRCodes for a Physical Inventory',
+      author='Eric Hanko',
+      author_email='v-erhank@microsoft.com',
+      packages=['pybar'],
+     )
