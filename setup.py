07729d1c9a6938c2639c8961e7f75aa08d8c1b19
Eric Hanko <v-erhank@microsoft.com>
2017-02-17 18:32:47 -0800

    Requirements, version number update, license - Update license to more reasonable version - Add license attribute - Add qrcode install requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='PyBar',
-      version='0.1',
+      version='0.0.1',
+      license='MIT',
       description='Generate QRCodes for a Physical Inventory',
       author='Eric Hanko',
       author_email='v-erhank@microsoft.com',
       packages=['pybar'],
       long_description=open('README.md').read(),
-     )
+      install_requires=[
+        "qrcode >= 5.3.0"])
