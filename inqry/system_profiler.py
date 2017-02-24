9a1946faf1bb0c775446fafeb10b193405f7a83b
Eric Hanko <v-erhank@microsoft.com>
2017-02-23 20:12:52 -0800

    include some exception handling

--- inqry/system_profiler.py
+++ inqry/system_profiler.py
@@ -5,16 +5,21 @@ import yaml
 BASE_COMMAND = '/usr/sbin/system_profiler'
 
 
-def _get_output_of_system_profiler_command(arguments=None):
+def hardware():
+    hardware_specs_dict = _hardware_parser(
+        _get_system_profiler_output(arguments='SPHardwareDataType'))
+    return hardware_specs_dict
+
+
+def _get_system_profiler_output(arguments=None):
     arguments = arguments or ''
     full_command = shlex.split(' '.join([BASE_COMMAND, arguments]))
     return subprocess.check_output(full_command).decode('utf-8')
 
 
 def _hardware_parser(system_profiler_output):
-    return yaml.load(system_profiler_output)['Hardware']['Hardware Overview']
-
-
-def hardware():
-    hardware_specs_dict = _get_output_of_system_profiler_command(arguments='SPHardwareDataType')
-    return hardware_specs_dict
+    hardware_dict = yaml.load(system_profiler_output)
+    try:
+        return hardware_dict['Hardware']['Hardware Overview']
+    except:
+        raise KeyError("Keys 'Hardware' and 'Hardware Overview' do not exist")
