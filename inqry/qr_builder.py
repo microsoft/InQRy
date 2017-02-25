6282fbb132987da6cdfc845af572ab73368efff2
Eric Hanko <v-erhank@microsoft.com>
2017-02-25 13:45:38 -0800

    Working qr_builder module - Implemented build_serial method into AssetQRCode class - Working to generate a qr code with only the serial added from SystemSpec object

--- inqry/qr_builder.py
+++ inqry/qr_builder.py
@@ -4,6 +4,17 @@ from qrcode import QRCode
 class AssetQRCode(QRCode):
     """TODO"""
 
-    def __init__(self):
-        """TODO"""
-        pass
+    def __init__(self, profile):
+        self.qr = QRCode()
+        super(AssetQRCode, self).__init__()
+        self.profile = profile
+
+    def build_serial(self):
+        self.qr.add_data(self.profile.serial)
+        return self.qr.make_image().show()
+
+    def build_more(self):
+        for prop in self.profile:
+            self.qr.add_data(prop)
+            return self.qr
+        return self.qr.make_image.show()
