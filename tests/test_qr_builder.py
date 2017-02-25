98980362b9140affb93c1a47194837ad24aea5aa
Eric Hanko <v-erhank@microsoft.com>
2017-02-25 14:25:58 -0800

    add use of systemspec_object fixture

--- tests/test_qr_builder.py
+++ tests/test_qr_builder.py
@@ -1,10 +1,12 @@
+import pytest
+from tests.test_systemspecs import systemspec_object
 from inqry.qr_builder import AssetQRCode
 
 
 def test_empty_asset_qr_code_can_be_instantiated():
-    AssetQRCode()
+    AssetQRCode(systemspec_object)
 
 
 def test_asset_qr_code_as_attributes_of_inherited_class():
-    qr = AssetQRCode()
+    qr = AssetQRCode(systemspec_object)
     assert hasattr(qr, 'add_data')
