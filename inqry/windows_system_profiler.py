363579554325c98d56118e32f1f8854a188cd7af
Eric Hanko <v-erhank@microsoft.com>
2017-02-27 23:46:39 -0800

    add skeleton code

--- inqry/windows_system_profiler.py
+++ inqry/windows_system_profiler.py
@@ -3,40 +3,37 @@ import wmi
 
 c = wmi.WMI()
 
-SYSINFO = c.Win32_ComputerSystem()[0]
-OSINFO = c.Win32_OperatingSystem()[0]
-CPUINFO = c.Win32_Processor()[0]
-HDDINFO = c.Win32_LogicalDisk()[0]
-RAMINFO = c.Win32_PhysicalMemory()[0]
-
-
-RAMTOTAL = int(SYSINFO.TotalPhysicalMemory)
-HDDTOTAL = int(HDDINFO.size)
-RAMSIZE = round(RAMTOTAL)
-HDDSIZE = round(HDDTOTAL)
-
-os.system('cls')
-# print("Model: " + MANUFACTURER + " " + MODEL)
-# print("\r")
-# print("HDD: " + str(HDDTOTAL) + "GB")
-# print("RAM: " + str(RAMTOTAL) + "GB")
-# print("CPU: " + CPUINFO.name)
-# print("OS: " + OSINFO.caption)
-
-
-print()
-
-
-def serial():
-    serial = c.Win32_BIOS()[0].SerialNumber
-    return print(serial)
+bios_output = c.Win32_BIOS()[0]
+processor_output = c.Win32_Processor()[0]
+disk_drive_output = c.Win32_DiskDrive()[0]
+logical_disk_output = c.Win32_LogicalDisk()[0]
+computer_system_output = c.Win32_ComputerSystem()[0]
+physical_memory_output = c.Win32_PhysicalMemory()[0]
 
 
 def manufacturer():
-    sysinfo = c.Win32_ComputerSystem()[0].Manufacturer
-    return sysinfo.Manufacturer
+    pass
+
+
+def name():
+    pass
 
 
 def model():
-    sysinfo = c.Win32_ComputerSystem()[0].Model
-    return sysinfo.Model
+    pass
+
+
+def serial():
+    pass
+
+
+def cpu():
+    pass
+
+
+def memory():
+    pass
+
+
+def storage():
+    pass
