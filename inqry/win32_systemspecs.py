17e8926aa34781fe40e984d1e47fbbc4bdad38ff
Eric Hanko <v-erhank@microsoft.com>
2017-02-27 17:55:06 -0800

    renamed wmi to win32 to avoid conflict

---
+++ inqry/win32_systemspecs.py
@@ -0,0 +1,33 @@
+import os
+import wmi
+
+c = wmi.WMI()
+
+SYSINFO = c.Win32_ComputerSystem()[0]
+OSINFO = c.Win32_OperatingSystem()[0]
+CPUINFO = c.Win32_Processor()[0]
+HDDINFO = c.Win32_LogicalDisk()[0]
+RAMINFO = c.Win32_PhysicalMemory()[0]
+
+MANUFACTURER = SYSINFO.Manufacturer
+MODEL = SYSINFO.Model
+RAMTOTAL = int(SYSINFO.TotalPhysicalMemory)
+HDDTOTAL = int(HDDINFO.size)
+RAMSIZE = round(RAMTOTAL)
+HDDSIZE = round(HDDTOTAL)
+
+os.system('cls')
+# print("Model: " + MANUFACTURER + " " + MODEL)
+# print("\r")
+# print("HDD: " + str(HDDTOTAL) + "GB")
+# print("RAM: " + str(RAMTOTAL) + "GB")
+# print("CPU: " + CPUINFO.name)
+# print("OS: " + OSINFO.caption)
+
+
+print()
+
+
+def serial():
+    bios = c.Win32_BIOS()[0]
+    return bios.SerialNumber
