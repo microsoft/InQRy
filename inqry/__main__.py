d825b45d034702afdcd6b914a3ab388da4a379c2
Eric Hanko <v-erhank@microsoft.com>
2017-03-01 17:40:57 -0800

    Testing JSON in main.py

--- inqry/__main__.py
+++ inqry/__main__.py
@@ -1,7 +1,23 @@
+import shelve
+import pickle
+import json
+from inqry.system_specs import macdisk as md
 from inqry.system_specs import mac_system_profiler as sp
 from inqry.system_specs import systemspecs
 
+
+def main():
+    specs = sp.hardware()
+    print(specs)
+    jspecs = json.dumps(specs, sort_keys=True, indent=4, separators=(';', ' = '))
+    print(jspecs)
+    disks = md.get_all_physical_disks()
+    print(disks)
+
+
 if __name__ == '__main__':
-    ss = systemspecs.mac_os()
-    print(ss.cpu_name)
-    print(sp.hardware())
+    main()
+
+
+# USER JSON LOAD FOR WINDOWS DATA ???
+# https://docs.python.org/3.6/library/json.html
