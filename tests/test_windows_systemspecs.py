c17578fdb675a689cdbe6b709861408eb11bd9a8
Eric Hanko <v-erhank@microsoft.com>
2017-03-01 23:39:21 -0800

    Create new windows_systemspec test
    - Replace mac dict with windows dict output

--- tests/test_windows_systemspecs.py
+++ tests/test_windows_systemspecs.py
@@ -1,4 +1,7 @@
-windows_hw_test_data = {
+import pytest
+from inqry.system_specs import systemspecs
+
+hw_test_data = {
     'Model Name': 'Surface_Pro_3',
     'Manufacturer': 'Microsoft Corporation',
     'Serial Number (system)': '000048250353',
@@ -7,21 +10,6 @@ windows_hw_test_data = {
     'Total Number of Cores': 2,
     'Processor Speed': 'Intel(R) Core(TM) i7-4650U CPU @ 1.70GHz',
     'Memory': '8 GB'}
-
-import pytest
-from inqry.system_specs import systemspecs
-
-hw_test_data = {'Model Name': 'Mac Pro', 'Model Identifier': 'MacPro6,1',
-                'Processor Name': 'Quad-Core Intel Xeon E5',
-                'Processor Speed': '3.7 GHz', 'Number of Processors': 1,
-                'Total Number of Cores': 4,
-                'L2 Cache (per Core)': '256 KB', 'L3 Cache': '10 MB',
-                'Memory': '32 GB',
-                'Boot ROM Version': 'MP61.0116.B21',
-                'SMC Version (system)': '2.20f18',
-                'Illumination Version': '1.4a6',
-                'Serial Number (system)': 'F5KQH0P9F9VN',
-                'Hardware UUID': '4D4C19C7-19C4-5678-A936-A419C4609AFD'}
 
 
 # noinspection PyShadowingNames
@@ -34,7 +22,7 @@ def systemspec_object():
 
 # noinspection PyShadowingNames
 def test_getting_value_from_key():
-    assert hw_test_data.get('Boot ROM Version') == 'MP61.0116.B21'
+    assert hw_test_data.get('Manufacturer') == 'Microsoft Corporation'
 
 
 # noinspection PyShadowingNames
