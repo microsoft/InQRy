2e72e6ff0f6065fd736f45889f29883c7a1181d8
Eric Hanko <v-erhank@microsoft.com>
2017-03-02 14:32:24 -0800

    Move GUI functionality to __main__ module of inqry

--- inqry/__main__.py
+++ inqry/__main__.py
@@ -1,18 +1,31 @@
-import shelve
-import pickle
-import json
-from inqry.system_specs import macdisk as md
-from inqry.system_specs import mac_system_profiler as sp
+import tkinter as tk
+from inqry.qr_builder import AssetQRCode
 from inqry.system_specs import systemspecs
+
+asset = AssetQRCode(systemspecs.main())
+root = tk.Tk()
+
+
+def center():  # Not used at this time
+    root.update_idletasks()
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    size = tuple(int(_) for _ in root.geometry().split('+')[0].split('x'))
+    x = screen_width / 2 - size[0] / 2
+    y = screen_height / 2 - size[1] / 2
+    root.geometry("%dx%d+%d+%d" % (size + (x, y)))
+
+
+def click():
+    asset.display()
 
 
 def main():
-    specs = sp.hardware()
-    print(specs)
-    jspecs = json.dumps(specs, sort_keys=True, indent=4)
-    print(jspecs)
-    disks = md.get_all_physical_disks()
-    print(disks)
+    root.title("InQRy")
+    button = tk.Button(root, text="Generate QR Code", command=click)
+    button.grid(column=1, row=0)
+    root.focus_force()
+    root.mainloop()
 
 
 if __name__ == '__main__':
