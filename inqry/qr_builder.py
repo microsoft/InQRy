0f368cda78a56df738f263edb149f628fc093ebd
Eric Hanko <v-erhank@microsoft.com>
2017-03-02 14:31:15 -0800

    Add docstring and display method

--- inqry/qr_builder.py
+++ inqry/qr_builder.py
@@ -2,15 +2,21 @@ from qrcode import QRCode
 
 
 class AssetQRCode(QRCode):
-    """TODO"""
+    """An AssetQRCode instance generates and displays a QR code. At the time
+    of writing this. The QR code is built only by being passed a list of string
+     objects using the add_data() method"""
 
     def __init__(self, profile):
         self.qr = QRCode()
         super(AssetQRCode, self).__init__()
         self.profile = profile
+        self.code = self.build()
 
     def build(self):
         attribute_list = self.profile.list_all()
         for attribute in attribute_list:
             self.qr.add_data(attribute)
+        return self.qr
+
+    def display(self):
         return self.qr.make_image().show()
