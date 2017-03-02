e37c446a64dc1d23022658ac26dc5840cc1bcd67
Eric Hanko <v-erhank@microsoft.com>
2017-03-01 20:30:29 -0800

    windows system profiler updates

--- inqry/__main__.py
+++ inqry/__main__.py
@@ -9,7 +9,7 @@ from inqry.system_specs import systemspecs
 def main():
     specs = sp.hardware()
     print(specs)
-    jspecs = json.dumps(specs, sort_keys=True, indent=4, separators=(';', ' = '))
+    jspecs = json.dumps(specs, sort_keys=True, indent=4)
     print(jspecs)
     disks = md.get_all_physical_disks()
     print(disks)
@@ -17,7 +17,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# USER JSON LOAD FOR WINDOWS DATA ???
-# https://docs.python.org/3.6/library/json.html
