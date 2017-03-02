479f199551aea0a29ab1c70f8c28823e8a21cc3f
Eric Hanko <v-erhank@microsoft.com>
2017-03-01 23:21:54 -0800

    begin x-platform systemspecs testing

--- tests/windows_system_profiler/test_windows_system_profiler.py
+++
@@ -1,5 +0,0 @@
-windows_system_profiler_output = """
-{'Model Name': 'Surface_Pro_3', 'Manufacturer': 'Microsoft Corporation', 'Serial Number (system)': '000048250353',
-'Model Identifier': 'Surface Pro 3', 'Number of Processors': 1, 'Total Number of Cores': 2,
-'Processor Speed': 'Intel(R) Core(TM) i7-4650U CPU @ 1.70GHz', 'Memory': '8 GB'}
-"""
