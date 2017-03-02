faeb1ea9eab529c44ce6e6ceb16340208e4ed2ca
Eric Hanko <v-erhank@microsoft.com>
2017-03-02 14:31:35 -0800

    Remove comments from __init__ of inqry package

--- inqry/__init__.py
+++ inqry/__init__.py
@@ -1,9 +1,3 @@
-# import sys
-
-# from inqry import main
-
-# descriptions
-
 __author__ = "OXO Hub Lab"
 __copyright__ = "Copyright 2017, Microsoft Corporation"
 __credits__ = ["Eric Hanko", "Jacob Zaval", "Michael Brown"]
