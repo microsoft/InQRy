90aa1009e1d6eb06c8f7e48f1eb2a48f1afd7877
Eric Hanko <v-erhank@microsoft.com>
2017-03-03 15:37:37 -0800

    Remove variables / inlined the returns

--- integration_tests/test_getting_windows_data.py
+++ integration_tests/test_getting_windows_data.py
@@ -1,16 +1,13 @@
-from inqry.system_specs import windows_system_profiler
+from inqry.system_specs import windows_system_profiler as wsp
 
 
 def test_hardware_function_returns_dictionary():
-    result = windows_system_profiler.hardware()
-    assert isinstance(result, dict)
+    assert isinstance(wsp.collector(), dict)
 
 
 def test_human_readable_functionality():
-    win = windows_system_profiler.WindowsProfile()
-    assert win.human_readable(46523456754) == "47 GB"
+    assert wsp.WindowsProfile().human_readable(46523456754) == "47 GB"
 
 
 def test_username_parses_correctly():
-    win = windows_system_profiler.WindowsProfile()
-    assert win.split_name("APEX35-2345\\Bob Saget") == "Bob Saget"
+    assert wsp.WindowsProfile().split_name("APEX35-2345\\Johnny Appleseed") == "Johnny Appleseed"
