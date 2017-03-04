406978f8db4c63ca8c816765f30060d60bf0af70
Eric Hanko <v-erhank@microsoft.com>
2017-03-03 17:04:41 -0800

    Remove unused code

--- tests/test_qr_builder.py
+++ tests/test_qr_builder.py
@@ -2,17 +2,11 @@ import pytest
 from tests.test_mac_systemspecs import systemspec_object
 from inqry.qr_builder import AssetQRCode
 
-sp = systemspec_object()
-
-
-def test_sp_is_class_instance():
-    pass
-
 
 def test_empty_asset_qr_code_can_be_instantiated():
-    assert AssetQRCode(sp)
+    assert AssetQRCode(systemspec_object())
 
 
 def test_asset_qr_code_as_attributes_of_inherited_class():
-    qr = AssetQRCode(sp)
+    qr = AssetQRCode(systemspec_object())
     assert hasattr(qr, 'add_data')
