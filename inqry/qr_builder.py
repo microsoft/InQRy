06edefba67789d2454581a644937d31ff7b9bae4
Jacob Zaval <jazava@microsoft.com>
2017-03-13 16:44:05 -0700

    update for instructions design paradigm

--- inqry/qr_builder.py
+++ inqry/qr_builder.py
@@ -1,5 +1,5 @@
 from qrcode import QRCode
-from instructions import Instructions
+from form_instructions import Instructions
 
 
 class AssetQRCode(QRCode):
@@ -7,14 +7,14 @@ class AssetQRCode(QRCode):
     of writing this. The QR code is built only by being passed a list of string
      objects using the add_data() method"""
 
-    def __init__(self, profile):
+    def __init__(self, instructions):
         self.qr = QRCode()
         super(AssetQRCode, self).__init__()
-        self.profile = profile
+        self.Instructions = instructions
         self.code = self.build()
 
     def build(self):
-        instructions = Instructions(self.profile.model).instructions()
+        instructions = self.Instructions.instruction_steps()
         for step in instructions:
             self.qr.add_data(step)
         return self.qr
