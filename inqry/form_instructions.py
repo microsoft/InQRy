e7352e5f094ea2834a1bc4bea529c99985fd0e32
Jacob Zaval <jazava@microsoft.com>
2017-03-14 11:27:48 -0700

    define fieldsets, models, and common fields

--- inqry/form_instructions.py
+++ inqry/form_instructions.py
@@ -1,7 +1,6 @@
 class Instructions:
-    """Create Instructions object"""
 
-    def __init__(self, specs):
+    def __init__(self, specs, user):
         self.specs = specs
         self.delay = 'delay500ms'
         self.space = '\x20'
@@ -16,8 +15,32 @@ class Instructions:
         self.drive3 = self.specs.drive3
         self.drive4 = self.specs.drive4
         self.serial = self.specs.serial
-        self.user = 'jazava'
-        self.assettag = '64655638'
+        self.user = user
+        self.model_definitions = {"MacPro6,1":"Desktop", "MacBookPro13,3":"Desktop"}
+        self.fieldsets = {"Desktop": (
+                                        self._text_box(self.processor) +
+                                        self._text_box(self.memory) +
+                                        self._text_box(self.drive1) +
+                                        self._text_box(self.drive2) +
+                                        self._text_box(self.drive3) +
+                                        self._text_box(self.drive4)
+                                        ),
+
+                            "Portable": (
+                                        self._text_box(self.processor) +
+                                        self._text_box(self.memory) +
+                                        self._text_box(self.drive1)
+                                        )
+        }
+
+    def _common_fields(self, unique_fields):
+        return (
+                self._list_box(self.model) +
+                unique_fields +
+                self._list_box(self.status) +
+                self._list_box(self.user) +
+                self._text_box(self.serial)
+                )
 
     def _text_box(self, field):
         return [field, self.tab]
@@ -26,15 +49,4 @@ class Instructions:
         return [self.space, self.delay, field, self.delay, self.select, self.delay, self.tab]
 
     def instruction_steps(self):
-        return (self._text_box(self.assettag) +
-                self._list_box(self.model) +
-                self._text_box(self.processor) +
-                self._text_box(self.memory) +
-                self._text_box(self.drive1) +
-                self._text_box(self.drive2) +
-                self._text_box(self.drive3) +
-                self._text_box(self.drive4) +
-                self._list_box(self.status) +
-                self._list_box(self.user) +
-                self._text_box(self.serial)
-                )
+        return self._common_fields(self.fieldsets[self.model_definitions[self.model]])
