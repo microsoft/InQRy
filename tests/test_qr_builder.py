0f8bad4f0e067dc0b6f5a033dab9a9e84e93103b
Jacob Zaval <jazava@microsoft.com>
2017-03-22 12:17:53 -0700

    Update qr_builder tests for new paradigm

--- tests/test_qr_builder.py
+++ tests/test_qr_builder.py
@@ -1,12 +1,12 @@
 import pytest
-from tests.test_mac_systemspecs import systemspec_object
+from tests.test_form_instructions import instructions_object
 from inqry.qr_builder import AssetQRCode
 
 
 def test_empty_asset_qr_code_can_be_instantiated():
-    assert AssetQRCode(systemspec_object())
+    assert AssetQRCode(instructions_object())
 
 
 def test_asset_qr_code_as_attributes_of_inherited_class():
-    qr = AssetQRCode(systemspec_object())
+    qr = AssetQRCode(instructions_object())
     assert hasattr(qr, 'add_data')
