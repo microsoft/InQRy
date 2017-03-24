bc167e30afbb7f70b51481fc24007fa841e7be37
Jacob Zaval <jazava@microsoft.com>
2017-03-24 10:31:17 -0700

    Remove unneeded assignments

--- inqry/form_instructions.py
+++ inqry/form_instructions.py
@@ -1,20 +1,19 @@
 class Instructions:
 
     def __init__(self, specs, user):
-        self.specs = specs
         self.delay = 'delay500ms'
         self.space = '\x20'
         self.tab = '\x09'
         self.select = 'enter_key'
         self.status = 'Ready'
-        self.model = self.specs.model
-        self.processor = "{} {}".format(self.specs.cpu_speed, self.specs.cpu_name)
-        self.memory = self.specs.memory
-        self.drive1 = self.specs.drive1
-        self.drive2 = self.specs.drive2
-        self.drive3 = self.specs.drive3
-        self.drive4 = self.specs.drive4
-        self.serial = self.specs.serial
+        self.model = specs.model
+        self.processor = "{} {}".format(specs.cpu_speed, specs.cpu_name)
+        self.memory = specs.memory
+        self.drive1 = specs.drive1
+        self.drive2 = specs.drive2
+        self.drive3 = specs.drive3
+        self.drive4 = specs.drive4
+        self.serial = specs.serial
         self.user = user
         self.model_definitions = {"MacBookPro13,3":"Portable", # MacBook Pro (15-inch, 2016)
                                   "MacBookPro13,2":"Portable", # MacBook Pro (13-inch, 2016, Four Thunderbolt 3 Ports)
