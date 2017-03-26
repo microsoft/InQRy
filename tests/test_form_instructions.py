1bacb70027a1179c1749ce6dbc3b47c0dd08974d
Eric Hanko <v-erhank@microsoft.com>
2017-03-25 17:02:24 -0700

    fix instructions test

--- tests/test_form_instructions.py
+++ tests/test_form_instructions.py
@@ -1,11 +1,11 @@
 import pytest
-from inqry.form_instructions import Instructions
-from inqry.system_specs.systemspecs import SystemSpecs
-from tests import test_mac_systemspecs
+from inqry import form_instructions
+from tests.mac import test_mac_systemspecs
 
 TEST_SPECS = test_mac_systemspecs.systemspec_object()
 USER = 'jazava'
 
+
 @pytest.fixture(scope='session')
 def instructions_object():
-    return Instructions.create_instructions_from_system_specs(TEST_SPECS, USER)
+    return form_instructions.create_instructions_from_system_specs(TEST_SPECS, USER)
