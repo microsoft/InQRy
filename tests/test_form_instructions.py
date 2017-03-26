964eb87770ae8d25c2944c2aeacceccfe6ebfc45
Eric Hanko <v-erhank@microsoft.com>
2017-03-25 17:17:12 -0700

    add two tests to test_instructions

--- tests/test_form_instructions.py
+++ tests/test_form_instructions.py
@@ -9,3 +9,11 @@ USER = 'jazava'
 @pytest.fixture(scope='session')
 def instructions_object():
     return form_instructions.create_instructions_from_system_specs(TEST_SPECS, USER)
+
+
+def test_instructions_object_instantiates():
+    instructions_object()
+
+
+def test_instructions_object_returns_size():
+    assert instructions_object().model == 'MacPro6,1'
