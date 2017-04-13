1efdb8c3aa71d3e8bdc8bc6ff5f5b5c08c1fc618
Eric Hanko <v-erhank@microsoft.com>
2017-04-13 09:00:22 -0700

    add tests for fusion drives

---
+++ tests/mac/diskutil/test_getting_identifiers_from_fusion_drive.py
@@ -0,0 +1,29 @@
+from inqry.system_specs import diskutil
+
+DISKUTIL_LIST_OUTPUT = """/dev/disk0 (internal, physical):
+   #:                       TYPE NAME                    SIZE       IDENTIFIER
+   0:      GUID_partition_scheme                        *121.3 GB   disk0
+   1:                        EFI EFI                     209.7 MB   disk0s1
+   2:          Apple_CoreStorage Macintosh HD            121.0 GB   disk0s2
+   3:                 Apple_Boot Boot OS X               134.2 MB   disk0s3
+
+/dev/disk1 (internal, physical):
+   #:                       TYPE NAME                    SIZE       IDENTIFIER
+   0:      GUID_partition_scheme                        *3.0 TB     disk1
+   1:                        EFI EFI                     209.7 MB   disk1s1
+   2:          Apple_CoreStorage Macintosh HD            289.5 GB   disk1s2
+   3:                 Apple_Boot Recovery HD             650.0 MB   disk1s3
+   4:                  Apple_HFS Data                    2.7 TB     disk1s4
+
+/dev/disk2 (internal, virtual):
+   #:                       TYPE NAME                    SIZE       IDENTIFIER
+   0:                  Apple_HFS Macintosh HD           +404.6 GB   disk2
+                                 Logical Volume on disk0s2, disk1s2
+                                 876DB79C-8794-4804-A4F2-B6D168CADECB
+                                 Unencrypted Fusion Drive
+
+/dev/disk3 (external, physical):
+   #:                       TYPE NAME                    SIZE       IDENTIFIER
+   0:      GUID_partition_scheme                        *8.0 GB     disk3
+   1:                        EFI EFI                     209.7 MB   disk3s1
+   2:                  Apple_HFS InQRy                   7.7 GB     disk3s2"""
---
+++ tests/mac/macdisk/test_representing_fusion_drive.py
@@ -0,0 +1,34 @@
+from inqry.system_specs import macdisk
+
+DISKUTIL_INFO = '''
+   Device Identifier:        disk0
+   Device Node:              /dev/disk0
+   Whole:                    Yes
+   Part of Whole:            disk0
+   Device / Media Name:      APPLE SSD SM0128G
+
+   Volume Name:              Not applicable (no file system)
+   Mounted:                  Not applicable (no file system)
+   File System:              None
+
+   Content (IOContent):      GUID_partition_scheme
+   OS Can Be Installed:      No
+   Media Type:               Generic
+   Protocol:                 PCI
+   SMART Status:             Verified
+
+   Disk Size:                121.3 GB (121332826112 Bytes) (exactly 236978176 512-Byte-Units)
+   Device Block Size:        512 Bytes
+
+   Read-Only Media:          No
+   Read-Only Volume:         Not applicable (no file system)
+
+   Device Location:          Internal
+   Removable Media:          Fixed
+
+   Solid State:              Yes
+   Virtual:                  No
+   OS 9 Drivers:             No
+   Low Level Format:         Not supported
+   Device Location:          "SSD"
+'''
