83a7128a42cf772e72505603568839f1c56d80db
Eric Hanko <v-erhank@microsoft.com>
2017-04-14 16:05:39 -0700

    Remove pytest fixture from test_mac_systemspecs.py

--- tests/mac/test_mac_systemspecs.py
+++ tests/mac/test_mac_systemspecs.py
@@ -7,87 +7,67 @@ HW_TEST_DATA = {'Model Name': 'Mac Pro', 'Model Identifier': 'MacPro6,1', 'Proce
                 'Boot ROM Version': 'MP61.0116.B21', 'SMC Version (system)': '2.20f18', 'Illumination Version': '1.4a6',
                 'Serial Number (system)': 'F5KQH0P9F9VN', 'Hardware UUID': '4D4C19C7-19C4-5678-A936-A419C4609AFD'}
 
-
-# noinspection PyShadowingNames
-@pytest.fixture(scope='session')
-def systemspec_object():
-    """Used as data for testing with a system profiler object"""
-    return systemspecs.create_specs_from_system_profiler_hardware_output(HW_TEST_DATA)
+SYSTEM_SPECS = systemspecs.create_specs_from_system_profiler_hardware_output(HW_TEST_DATA)
 
 
-# noinspection PyShadowingNames
 def test_getting_value_from_key():
     assert HW_TEST_DATA.get('Boot ROM Version') == 'MP61.0116.B21'
 
 
-# noinspection PyShadowingNames
 def test_profile_instantiation_works():
     systemspecs.SystemSpecs(HW_TEST_DATA)
 
 
-# noinspection PyShadowingNames
-def test_operating_system_attribute(systemspec_object):
-    assert hasattr(systemspec_object, "os_type")
+def test_operating_system_attribute():
+    assert hasattr(SYSTEM_SPECS, "os_type")
 
 
-@pytest.mark.skip
-def test_that_system_profile_object_has_storage_attribute(systemspec_object):
-    assert hasattr(systemspec_object, "storage")
+def test_that_system_profile_object_has_storage_attribute():
+    assert hasattr(SYSTEM_SPECS, "storage")
 
 
-# noinspection PyShadowingNames
-def test_that_system_profile_object_has_serial_attribute(systemspec_object):
-    assert hasattr(systemspec_object, "serial")
+def test_that_system_profile_object_has_serial_attribute():
+    assert hasattr(SYSTEM_SPECS, "serial")
 
 
-# noinspection PyShadowingNames
-def test_that_system_profile_object_has_cpu_name_attribute(systemspec_object):
-    assert hasattr(systemspec_object, "cpu_name")
+def test_that_system_profile_object_has_cpu_name_attribute():
+    assert hasattr(SYSTEM_SPECS, "cpu_name")
 
 
-# noinspection PyShadowingNames
-def test_that_system_profile_object_has_cpu_speed_attribute(systemspec_object):
-    assert hasattr(systemspec_object, "cpu_speed")
+def test_that_system_profile_object_has_cpu_speed_attribute():
+    assert hasattr(SYSTEM_SPECS, "cpu_speed")
 
 
-# noinspection PyShadowingNames
-def test_that_system_profile_object_has_cpu_processors_attribute(systemspec_object):
-    assert hasattr(systemspec_object, "cpu_processors")
+def test_that_system_profile_object_has_cpu_processors_attribute():
+    assert hasattr(SYSTEM_SPECS, "cpu_processors")
 
 
-# noinspection PyShadowingNames
-def test_system_profiler_has_os_type_attribute(systemspec_object):
-    assert bool(systemspec_object.os_type) is True
+def test_system_profiler_has_os_type_attribute():
+    assert bool(SYSTEM_SPECS.os_type) is True
 
 
-# noinspection PyShadowingNames
-def test_model_is_string(systemspec_object):
-    assert isinstance(systemspec_object.model, str)
+def test_model_is_string():
+    assert isinstance(SYSTEM_SPECS.model, str)
 
 
-# noinspection PyShadowingNames
-def test_serial_is_string(systemspec_object):
-    assert isinstance(systemspec_object.serial, str)
+def test_serial_is_string():
+    assert isinstance(SYSTEM_SPECS.serial, str)
 
 
-# noinspection PyShadowingNames
-def test_name_is_string(systemspec_object):
-    assert isinstance(systemspec_object.cpu_name, str)
+def test_name_is_string():
+    assert isinstance(SYSTEM_SPECS.cpu_name, str)
 
 
-# noinspection PyShadowingNames
-def test_processors_is_integer(systemspec_object):
-    assert isinstance(systemspec_object.cpu_processors, int)
+def test_processors_is_integer():
+    assert isinstance(SYSTEM_SPECS.cpu_processors, int)
 
 
-# noinspection PyShadowingNames
-def test_speed_is_string(systemspec_object):
-    assert isinstance(systemspec_object.cpu_speed, str)
+def test_speed_is_string():
+    assert isinstance(SYSTEM_SPECS.cpu_speed, str)
 
 
-# noinspection PyShadowingNames
-def test_cores_is_integer(systemspec_object):
-    assert isinstance(systemspec_object.cpu_cores, int)
+def test_cores_is_integer():
+    assert isinstance(SYSTEM_SPECS.cpu_cores, int)
 
 
 @pytest.mark.skip
@@ -102,9 +82,9 @@ def test_ability_to_get_components_from_system_profile_object():
 
 @pytest.mark.skip
 def test_collector_method_output_data_type_is_system_specs_class():
-    assert isinstance(systemspec_object.collector(), systemspecs.SystemSpecs)
+    assert isinstance(SYSTEM_SPECS.collector(), systemspecs.SystemSpecs)
 
 
 @pytest.mark.skip
-def test_if_disk_list_is_list(systemspec_object):
-    assert isinstance(systemspec_object.storage(), list)
+def test_if_disk_list_is_list():
+    assert isinstance(SYSTEM_SPECS.storage(), list)
