be1916235baefc98efee7144ad8ed9cefa716a95
Eric Hanko <v-erhank@microsoft.com>
2017-04-19 17:33:47 -0700

    Prepare for new all-encompassing systemspecs unit test file

--- tests/test_form_instructions.py
+++ tests/test_form_instructions.py
@@ -1,16 +1,17 @@
-import pytest
-from inqry import form_instructions
-from tests.mac.test_mac_systemspecs import SYSTEM_SPECS
-
-
-@pytest.fixture(scope='session')
-def instructions_object():
-    return form_instructions.Instructions(SYSTEM_SPECS)
-
-
-def test_instructions_object_instantiates():
-    instructions_object()
-
-
-def test_instructions_object_returns_size():
-    assert instructions_object().model == 'MacPro6,1'
+# import pytest
+#
+# from inqry import form_instructions
+# from tests.test_mac_systemspecs import SYSTEM_SPECS
+#
+#
+# @pytest.fixture(scope='session')
+# def instructions_object():
+#     return form_instructions.Instructions(SYSTEM_SPECS)
+#
+#
+# def test_instructions_object_instantiates():
+#     instructions_object()
+#
+#
+# def test_instructions_object_returns_size():
+#     assert instructions_object().model == 'MacPro6,1'
