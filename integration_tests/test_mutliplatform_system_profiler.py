304fdee91318fde41ce6dfb1267e0dc3ec66d800
Eric Hanko <v-erhank@microsoft.com>
2017-04-19 18:30:23 -0700

    Rename collector to get_hardware_overview

--- integration_tests/test_mutliplatform_system_profiler.py
+++ integration_tests/test_mutliplatform_system_profiler.py
@@ -2,7 +2,7 @@ from inqry.system_specs import system_profiler
 
 
 def test_hardware_function_returns_dictionary():
-    assert system_profiler.collector()
+    assert system_profiler.get_hardware_overview()
 
 
 def test_get_internal_storage_is_list():
