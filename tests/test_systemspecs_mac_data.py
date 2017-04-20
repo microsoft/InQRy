55603cfebbadd1e347c6a5e81f0773d6804f2633
Eric Hanko <v-erhank@microsoft.com>
2017-04-19 18:31:57 -0700

    storage argument for systemspecs - update test files for mac and windows

--- tests/test_systemspecs_mac_data.py
+++ tests/test_systemspecs_mac_data.py
@@ -1,18 +1,30 @@
 from inqry.system_specs import systemspecs
+from tests.mac.macdisk.test_representing_external_hdd_generic import test_disk as disk1
+from tests.mac.macdisk.test_representing_internal_ssd_touchbar import test_disk as disk2
 
-DATA = {'Model Name': 'Mac Pro',
-        'Model Identifier': 'MacPro6,1',
-        'Processor Name': 'Quad-Core Intel Xeon E5',
-        'Processor Speed': '3.7 GHz',
-        'Number of Processors': 1,
-        'Total Number of Cores': 4,
-        'L2 Cache (per Core)': '256 KB',
-        'L3 Cache': '10 MB',
-        'Memory': '32 GB',
-        'Boot ROM Version': 'MP61.0116.B21',
-        'SMC Version (system)': '2.20f18',
-        'Illumination Version': '1.4a6',
-        'Serial Number (system)': 'F5KQH0P9F9VN',
-        'Hardware UUID': '4D4C19C7-19C4-5678-A936-A419C4609AFD'}
+HARDWARE = {'Model Name': 'Mac Pro',
+            'Model Identifier': 'MacPro6,1',
+            'Processor Name': 'Quad-Core Intel Xeon E5',
+            'Processor Speed': '3.7 GHz',
+            'Number of Processors': 1,
+            'Total Number of Cores': 4,
+            'L2 Cache (per Core)': '256 KB',
+            'L3 Cache': '10 MB',
+            'Memory': '32 GB',
+            'Boot ROM Version': 'MP61.0116.B21',
+            'SMC Version (system)': '2.20f18',
+            'Illumination Version': '1.4a6',
+            'Serial Number (system)': 'F5KQH0P9F9VN',
+            'Hardware UUID': '4D4C19C7-19C4-5678-A936-A419C4609AFD'}
 
-SYSTEM_SPECS = systemspecs.SystemSpecs(DATA)
+INTERNAL_STORAGE = [disk1, disk2]
+
+SYSTEM_SPECS = systemspecs.SystemSpecs(HARDWARE, INTERNAL_STORAGE)
+
+
+def test_system_specs_can_be_instantiated_with_test_data():
+    assert SYSTEM_SPECS
+
+
+def test_drive_bay_1_returns_correct_data():
+    assert SYSTEM_SPECS.drive1 == '3.0 TB HDD (APPLE HDD ST3000DM001)'
--- tests/test_systemspecs_windows_data.py
+++ tests/test_systemspecs_windows_data.py
@@ -1,13 +1,24 @@
 from inqry.system_specs import systemspecs
+from tests.windows.windisk.test_creating_a_disk_from_get_physical_disk_output import test_disk as disk1
 
-DATA = {'Model Name': 'Surface_Pro_3',
-        'Manufacturer': 'Microsoft Corporation',
-        'Serial Number (system)': '000048250353',
-        'Model Identifier': 'Surface Pro 3',
-        'Number of Processors': 1,
-        'Total Number of Cores': 2,
-        'Processor Speed': 'Intel(R) Core(TM) i7-4650U CPU @ 1.70GHz',
-        'Memory': '8 GB',
-        'Processor Name': 'Intel(R) Core(TM) i7-4650U CPU @ 1.70GHz'}
+HARDWARE = {'Model Name': 'Surface_Pro_3',
+            'Manufacturer': 'Microsoft Corporation',
+            'Serial Number (system)': '000048250353',
+            'Model Identifier': 'Surface Pro 3',
+            'Number of Processors': 1,
+            'Total Number of Cores': 2,
+            'Processor Speed': 'Intel(R) Core(TM) i7-4650U CPU @ 1.70GHz',
+            'Memory': '8 GB',
+            'Processor Name': 'Intel(R) Core(TM) i7-4650U CPU @ 1.70GHz'}
 
-SYSTEM_SPECS = systemspecs.SystemSpecs(DATA)
+INTERNAL_STORAGE = [disk1]
+
+SYSTEM_SPECS = systemspecs.SystemSpecs(HARDWARE, INTERNAL_STORAGE)
+
+
+def test_system_specs_can_be_instantiated_with_test_data():
+    assert SYSTEM_SPECS
+
+
+def test_drive_bay_1_returns_correct_data():
+    assert SYSTEM_SPECS.drive1 == '512 GB SSD (SAMSUNG MZHPV512HDGL-000H1)'
