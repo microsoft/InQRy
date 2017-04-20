18ea489d3a7b8c9d60b61b4faf98c4051c5d6015
Eric Hanko <v-erhank@microsoft.com>
2017-04-19 17:45:47 -0700

    Start clean up of system specs data - separate files for each data test set

---
+++ tests/test_systemspecs_windows_data.py
@@ -0,0 +1,13 @@
+from inqry.system_specs import systemspecs
+
+DATA = {'Model Name': 'Surface_Pro_3',
+        'Manufacturer': 'Microsoft Corporation',
+        'Serial Number (system)': '000048250353',
+        'Model Identifier': 'Surface Pro 3',
+        'Number of Processors': 1,
+        'Total Number of Cores': 2,
+        'Processor Speed': 'Intel(R) Core(TM) i7-4650U CPU @ 1.70GHz',
+        'Memory': '8 GB',
+        'Processor Name': 'Intel(R) Core(TM) i7-4650U CPU @ 1.70GHz'}
+
+SYSTEM_SPECS = systemspecs.SystemSpecs(DATA)
