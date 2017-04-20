b03e2adab125034204d0b44d0507ba3d36fcc15c
Eric Hanko <v-erhank@microsoft.com>
2017-04-19 23:58:08 -0700

    Fix up the import of windows_system_profiler

--- tests/windows/windows_system_profiler/test_helper_functions.py
+++ tests/windows/windows_system_profiler/test_helper_functions.py
@@ -1,9 +1,9 @@
-from inqry.system_specs import windows_system_profiler as wsp
+from inqry.system_specs import windows_system_profiler
 
 
 def test_human_readable_functionality():
-    assert wsp.human_readable(46523456754) == "47 GB"
+    assert windows_system_profiler.human_readable(46523456754) == "47 GB"
 
 
 def test_username_parses_correctly():
-    assert wsp.split_name("APEX35-2345\\Johnny Appleseed") == "Johnny Appleseed"
+    assert windows_system_profiler.split_name("APEX35-2345\\Johnny Appleseed") == "Johnny Appleseed"
