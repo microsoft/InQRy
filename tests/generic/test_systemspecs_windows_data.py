a41d307d477ad6b3749e1afeb77bce47c65aa8ba
Eric Hanko <v-erhank@microsoft.com>
2017-04-24 14:56:29 -0700

    Update windows test file for new var name and OS type argument

--- tests/generic/test_systemspecs_windows_data.py
+++ tests/generic/test_systemspecs_windows_data.py
@@ -13,28 +13,30 @@ HARDWARE = {'Model Name': 'Surface_Pro_3',
 
 INTERNAL_STORAGE = [disk1]
 
-SYSTEM_SPECS = systemspecs.SystemSpecs(HARDWARE, INTERNAL_STORAGE)
+OS_TYPE = 'win32'
+
+WINDOWS_SYSTEM_SPECS = systemspecs.SystemSpecs(HARDWARE, INTERNAL_STORAGE, OS_TYPE)
 
 
 def test_system_specs_can_be_instantiated_with_test_data():
-    assert SYSTEM_SPECS
+    assert WINDOWS_SYSTEM_SPECS
 
 
 def test_assert_system_specs_storage():
-    assert SYSTEM_SPECS.storage
+    assert WINDOWS_SYSTEM_SPECS.storage
 
 
 def test_system_specs_drive_count_returns_correct_amount():
-    assert SYSTEM_SPECS.drive_count == 1
+    assert WINDOWS_SYSTEM_SPECS.drive_count == 1
 
 
 def test_drive_bay_1_returns_correct_data():
-    assert SYSTEM_SPECS.drive1 == '512 GB SSD (SAMSUNG MZHPV512HDGL-000H1)'
+    assert WINDOWS_SYSTEM_SPECS.drive1 == '512 GB SSD (SAMSUNG MZHPV512HDGL-000H1)'
 
 
 def test_cpu_speed_is_parsed_correctly():
-    assert SYSTEM_SPECS.cpu_speed == '1.70GHz'
+    assert WINDOWS_SYSTEM_SPECS.cpu_speed == '1.70GHz'
 
 
 def test_cpu_name_returns_name():
-    assert SYSTEM_SPECS.cpu_name == 'Intel(R) Core(TM) i7-4650U CPU'
+    assert WINDOWS_SYSTEM_SPECS.cpu_name == 'Intel(R) Core(TM) i7-4650U CPU'
