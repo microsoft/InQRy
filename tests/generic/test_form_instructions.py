fa09c4444c4094caf9bcfeecceb14300fcd35cb5
Eric Hanko <v-erhank@microsoft.com>
2017-04-24 14:52:52 -0700

    Add test for identify_model method; renamed var from systemspecs test

--- tests/generic/test_form_instructions.py
+++ tests/generic/test_form_instructions.py
@@ -1,13 +1,16 @@
 from inqry import form_instructions
-from tests.generic.test_systemspecs_mac_data import SYSTEM_SPECS
+from tests.generic.test_systemspecs_mac_data import MAC_SYSTEM_SPECS
 
-
-INSTRUCTIONS = form_instructions.Instructions(SYSTEM_SPECS)
+INSTRUCTIONS = form_instructions.Instructions(MAC_SYSTEM_SPECS)
 
 
 def test_instructions_object_instantiates():
-    assert form_instructions.Instructions(SYSTEM_SPECS)
+    assert form_instructions.Instructions(MAC_SYSTEM_SPECS)
 
 
 def test_instructions_object_returns_model():
     assert INSTRUCTIONS.model == 'MacPro6,1'
+
+
+def test_instructions_can_identify_type_of_model():
+    assert INSTRUCTIONS.identify_model() == 'Desktop'
