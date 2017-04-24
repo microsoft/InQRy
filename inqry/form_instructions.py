0f41b649c479459bad94ed9ce1c0ab437d0314b5
Eric Hanko <v-erhank@microsoft.com>
2017-04-24 16:39:57 -0700

    Remove form factor identification from form_instructions module

--- inqry/form_instructions.py
+++ inqry/form_instructions.py
@@ -1,6 +1,3 @@
-import re
-
-
 class FormInstructions:
     def __init__(self, specs, user=None):
         self.delay = '~d'
@@ -9,6 +6,7 @@ class FormInstructions:
         self.space = '\x20'
         self.status = 'Deploy'
         self.model = specs.model
+        self.form_factor = specs.form_factor
         self.processor = "{} {}".format(specs.cpu_speed, specs.cpu_name)
         self.memory = specs.memory
         self.drive1 = specs.drive1
@@ -22,18 +20,6 @@ class FormInstructions:
                                       self._text_box(self.drive3) + self._text_box(self.drive4)),
                           'Portable': (self._text_box(self.processor) + self._text_box(self.memory) +
                                        self._text_box(self.drive1))}
-        self.os = specs.os_type
-
-    def identify_model(self):
-        return self._identify_mac_model() if self.os == 'Darwin' else self._identify_pc_model()
-
-    def _identify_mac_model(self):
-        mac_portable_pattern = re.compile(r'MacBook(Pro|Air|)([0-9]|[1-9][0-9]),[1-9]')
-        return 'Portable' if re.match(mac_portable_pattern, self.model) else 'Desktop'
-
-    def _identify_pc_model(self):
-        pc_portable_pattern = re.compile(r'')
-        return 'Portable' if not re.match(pc_portable_pattern, self.model) else 'Desktop'
 
     def _common_fields(self, unique_fields):
         return (self._list_box(self.model) + unique_fields + self._list_box(self.status) +
@@ -46,4 +32,4 @@ class FormInstructions:
         return [self.space, self.delay, field, self.delay, self.enter, self.delay, self.tab]
 
     def instruction_steps(self):
-        return self._common_fields(self.fieldsets[self.identify_model()])
+        return self._common_fields(self.fieldsets[self.form_factor])
