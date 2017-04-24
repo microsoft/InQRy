0eabb7087f0854b69ff7a1c808571c6b1762e2e7
Eric Hanko <v-erhank@microsoft.com>
2017-04-24 16:44:47 -0700

    Remove data type test from asset_qrcode test module

--- tests/generic/test_asset_qrcode.py
+++ tests/generic/test_asset_qrcode.py
@@ -6,11 +6,3 @@ QR = AssetQRCode(INSTRUCTIONS)
 
 def test_empty_asset_qr_code_can_be_instantiated():
     assert QR
-
-
-def test_asset_qr_code_as_attributes_of_inherited_class():
-    assert hasattr(QR, 'add_data')
-
-
-def test_asset_qr_code_has_display_attr():
-    assert hasattr(QR, 'display')
