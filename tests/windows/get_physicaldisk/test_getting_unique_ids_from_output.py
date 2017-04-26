3002b1bbdd424de19627909372a48c88c02fed97
Eric Hanko <v-erhank@microsoft.com>
2017-04-26 00:45:37 -0700

    update get_physicaldisk to find more types of unique disk ids

--- tests/windows/get_physicaldisk/test_getting_unique_ids_from_output.py
+++ tests/windows/get_physicaldisk/test_getting_unique_ids_from_output.py
@@ -14,7 +14,11 @@ eui.0025384161B6798A
 
 
 def test_creating_list_of_unique_disk_ids():
-    expected_physical_disks = ['{256a2559-ce63-5434-1bee-3ff629daa3a7}', '{4069d186-f178-856e-cff3-ba250c28446d}',
-                               '{4da19f06-2e28-2722-a0fb-33c02696abcd}', '50014EE20D887D66', 'eui.0025384161B6798A',
-                               '500A07510F1A545C', '5000C5007A75E216']
-    assert expected_physical_disks == get_physicaldisk.get_physical_disk_identifiers(UNIQUE_ID_OUTPUT)
+    expected_physical_disks = {'{256a2559-ce63-5434-1bee-3ff629daa3a7}',
+                               '{4069d186-f178-856e-cff3-ba250c28446d}',
+                               '{4da19f06-2e28-2722-a0fb-33c02696abcd}',
+                               '50014EE20D887D66',
+                               'eui.0025384161B6798A',
+                               '5000C5007A75E216',
+                               '500A07510F1A545C'}
+    assert expected_physical_disks == set(get_physicaldisk.get_physical_disk_identifiers(UNIQUE_ID_OUTPUT))
