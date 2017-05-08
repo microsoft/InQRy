74438bb0db6d03ca4f52b98659ec6c817b98cd4b
Eric Hanko <v-erhank@microsoft.com>
2017-05-08 11:11:26 -0700

    rearrange grid

--- InQRy.py
+++ InQRy.py
@@ -11,7 +11,7 @@ class InQRyGUI:
         self.form_factor = tk.StringVar()
         self.new_model_selected = tk.IntVar()
         self.alias_entry = tk.Entry(self.win)
-        self.alias_entry.grid(row=0, column=1, pady=5)
+        self.alias_entry.grid(row=1, column=1, pady=5)
         self.systemspecs = systemspecs.SystemSpecs()
         self.create_widgets()
 
@@ -25,22 +25,22 @@ class InQRyGUI:
 
     def create_widgets(self):
         generate_qr_button = tk.Button(self.win, text='Generate QR Code', command=self.click)
-        generate_qr_button.grid(row=5, columnspan=2, pady=15)
+        generate_qr_button.grid(row=6, columnspan=2, pady=15)
 
         desktop_radio_button = tk.Radiobutton(self.win, text='Desktop', variable=self.form_factor, value='Desktop')
         desktop_radio_button.select()
-        desktop_radio_button.grid(row=1, columnspan=2)
+        desktop_radio_button.grid(row=2, columnspan=2)
 
         portable_radio_button = tk.Radiobutton(self.win, text='Portable', variable=self.form_factor, value='Portable')
         portable_radio_button.deselect()
-        portable_radio_button.grid(row=2, columnspan=2)
+        portable_radio_button.grid(row=3, columnspan=2)
 
         alias_label = tk.Label(self.win, text='Alias:')
-        alias_label.grid(row=0, column=0, pady=5, sticky=tk.E)
+        alias_label.grid(row=1, column=0, pady=5, sticky=tk.E)
 
         new_model_checkbutton = tk.Checkbutton(self.win, text='New Model', variable=self.new_model_selected)
         new_model_checkbutton.deselect()
-        new_model_checkbutton.grid(row=6, columnspan=2)
+        new_model_checkbutton.grid(row=0, columnspan=2)
 
 
 if __name__ == '__main__':
