f7468940723a5db5d5c3ff97f7816b8b1f147656
Eric Hanko <v-erhank@microsoft.com>
2017-05-08 09:22:14 -0700

    new model logic added

--- InQRy.py
+++ InQRy.py
@@ -9,21 +9,24 @@ class InQRyGUI:
         self.win = tk.Tk()
         self.win.title('InQRy')
         self.form_factor = tk.StringVar()
-        self.new_model_selected = tk.StringVar()
+        self.new_model_selected = tk.IntVar()
         self.alias_entry = tk.Entry(self.win)
         self.alias_entry.grid(row=0, column=1, pady=5)
         self.create_widgets()
 
     def click(self):
         data = FormInstructions(systemspecs.SystemSpecs(), self.form_factor.get(), self.alias_entry.get())
-        AssetQRCode(data).display()
+        if self.new_model_selected.get():
+            print('You want a new model?')
+        else:
+            AssetQRCode(data).display()
 
     def create_widgets(self):
         generate_qr_button = tk.Button(self.win, text='Generate QR Code', command=self.click)
         generate_qr_button.grid(row=5, columnspan=2, pady=15)
 
         desktop_radio_button = tk.Radiobutton(self.win, text='Desktop', variable=self.form_factor, value='Desktop')
-        desktop_radio_button.deselect()
+        desktop_radio_button.select()
         desktop_radio_button.grid(row=1, columnspan=2)
 
         portable_radio_button = tk.Radiobutton(self.win, text='Portable', variable=self.form_factor, value='Portable')
