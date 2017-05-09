b76db2e065002b8545b5efa13deb09387ead07e6
Eric Hanko <v-erhank@microsoft.com>
2017-05-09 10:17:50 -0700

    minor renaming refactors

--- InQRy.py
+++ InQRy.py
@@ -6,43 +6,43 @@ from inqry.form_instructions import FormInstructions
 
 class InQRyGUI:
     def __init__(self):
-        self.win = tk.Tk()
-        self.win.title('InQRy')
+        self.root_window = tk.Tk()
+        self.root_window.title('InQRy')
         self.form_factor = tk.StringVar()
         self.new_model_selected = tk.IntVar()
-        self.alias_entry = tk.Entry(self.win)
+        self.alias_entry = tk.Entry(self.root_window)
         self.alias_entry.grid(row=1, column=1, pady=5)
         self.systemspecs = systemspecs.SystemSpecs()
         self.create_widgets()
 
     def click(self):
         if self.new_model_selected.get():
-            data = FormInstructions(self.systemspecs, 'New Model')
-            AssetQRCode(data).display()
+            form_instructions = FormInstructions(self.systemspecs, 'New Model')
+            AssetQRCode(form_instructions).display()
         else:
-            data = FormInstructions(self.systemspecs, self.form_factor.get(), self.alias_entry.get())
-            AssetQRCode(data).display()
+            form_instructions = FormInstructions(self.systemspecs, self.form_factor.get(), self.alias_entry.get())
+            AssetQRCode(form_instructions).display()
 
     def create_widgets(self):
-        generate_qr_button = tk.Button(self.win, text='Generate QR Code', command=self.click)
+        generate_qr_button = tk.Button(self.root_window, text='Generate QR Code', command=self.click)
         generate_qr_button.grid(row=6, columnspan=2, pady=15)
 
-        desktop_radio_button = tk.Radiobutton(self.win, text='Desktop', variable=self.form_factor, value='Desktop')
+        desktop_radio_button = tk.Radiobutton(self.root_window, text='Desktop', variable=self.form_factor, value='Desktop')
         desktop_radio_button.select()
         desktop_radio_button.grid(row=2, columnspan=2)
 
-        portable_radio_button = tk.Radiobutton(self.win, text='Portable', variable=self.form_factor, value='Portable')
+        portable_radio_button = tk.Radiobutton(self.root_window, text='Portable', variable=self.form_factor, value='Portable')
         portable_radio_button.deselect()
         portable_radio_button.grid(row=3, columnspan=2)
 
-        alias_label = tk.Label(self.win, text='Alias:')
+        alias_label = tk.Label(self.root_window, text='Alias:')
         alias_label.grid(row=1, column=0, pady=5, sticky=tk.E)
 
-        new_model_checkbutton = tk.Checkbutton(self.win, text='New Model', variable=self.new_model_selected)
+        new_model_checkbutton = tk.Checkbutton(self.root_window, text='New Model', variable=self.new_model_selected)
         new_model_checkbutton.deselect()
         new_model_checkbutton.grid(row=0, columnspan=2)
 
 
 if __name__ == '__main__':
-    main = InQRyGUI()
-    main.win.mainloop()
+    gui = InQRyGUI()
+    gui.root_window.mainloop()
