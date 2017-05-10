f96e310964207e3e04934e60c7be0b3c3b263a1e
Eric Hanko <v-erhank@microsoft.com>
2017-05-09 19:33:33 -0700

    add integration test

---
+++ tests/integration/test_cfgutil_command.py
@@ -0,0 +1,7 @@
+from inqry.system_specs import cfgutil
+
+
+def test_cfgutil_output_has_correct_keys():
+    raw_json = cfgutil._get_output_of_cfgutil_command()
+    output = cfgutil.parse_command_output(raw_json)
+    assert str(output.keys()) == "dict_keys(['Command', 'Output', 'Type', 'Devices'])"
