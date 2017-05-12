961a3c0daa9bcbf7c05d49716388f5fb1487ad27
Eric Hanko <v-erhank@microsoft.com>
2017-05-11 23:15:41 -0700

    update ios integration tests

--- tests/integration/test_cfgutil_command.py
+++ tests/integration/test_cfgutil_command.py
@@ -1,26 +1,24 @@
 import pytest
 from inqry.system_specs import cfgutil
 
-hardware_overview = cfgutil.get_hardware_overview_for_all_devices()
-device_properties = cfgutil.get_device_properties_from_cfgutil_output()
-
 
 @pytest.mark.skip
 def test_get_hardware_overview_for_all_devices():
-    assert hardware_overview
+    assert cfgutil.get_hardware_overview_for_all_devices()
 
 
 @pytest.mark.skip
 def test_get_device_properties_for_all_devices():
-    assert device_properties
+    assert cfgutil.get_device_properties_from_cfgutil_output()
 
 
 @pytest.mark.skip
 def test_hardware_overview_keys_are_correct_for_attached_device():
-    first_device = hardware_overview[0]
+    first_device = cfgutil.get_hardware_overview_for_all_devices()[0]
     assert str(first_device.keys()) == "dict_keys(['serialNumber', 'totalDiskCapacity', 'IMEI', 'deviceType'])"
 
 
 @pytest.mark.skip
 def test_device_property_keys_are_correct_for_attached_device():
-    assert str(device_properties.keys()) == "dict_keys(['Command', 'Output', 'Type', 'Devices'])"
+    assert str(
+        cfgutil.get_device_properties_from_cfgutil_output().keys()) == "dict_keys(['Command', 'Output', 'Type', 'Devices'])"
