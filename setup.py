54f0d0713a0f6fb843a0ff330a0846bce78dca1f
Eric Hanko <v-erhank@microsoft.com>
2017-05-17 14:44:38 -0700

    update setup module

--- setup.py
+++ setup.py
@@ -1,38 +1,33 @@
-import sys
-
 try:
     from setuptools import setup
-except ModuleNotFoundError:
+except ImportError:
     from distutils.core import setup
 
-APP = ['InQRy.py']
-OPTIONS = {}
-
-if sys.platform == 'darwin':
-    extra_options = dict(
-            setup_requires=['py2app'],
-            install_requires=['PyYAML'],
-    )
-elif sys.platform == 'win32':
-    extra_options = dict(
-            install_requires=['wmi', 'pypiwin32'],
-    )
-
-setup(app=APP,
-      author=['Apex Lab'],
-      author_email='apxlab@microsoft.com',
-      description='A cross-platform utility used to generate a QR code containing hardware specs',
-      license='MIT',
-      long_description=open('README.md').read(),
-      name='InQRy',
-      options={'py2app': OPTIONS},
-      packages=['inqry', "inqry.system_specs", "tests"],
-      install_requires=['qrcode', 'Pillow'],
-      tests_require=['pytest'],
-      url='https://github.com/Microsoft/InQRy',
-      version='1.2.1',
-      classifiers=[
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6']
-      )
+setup(
+    name='inqry',
+    app='launcher.py',
+    author=['Apex Lab'],
+    author_email='apxlab@microsoft.com',
+    description='A cross-platform utility used to generate a QR code containing hardware specs',
+    license='MIT',
+    long_description=open('README.md').read(),
+    packages=['inqry', 'inqry.system_specs'],
+    install_requires=['Pillow',
+                      'pytest',
+                      'PyYAML',
+                      'wmi;platform_system=="Windows"',
+                      'pypiwin32;platform_system=="Windows"'],
+    tests_require=['pytest'],
+    url='https://github.com/Microsoft/InQRy',
+    version='1.2.1',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only']
+)
