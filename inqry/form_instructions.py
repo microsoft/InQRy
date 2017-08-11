eecbc6749c0f702f2d589d23066be84a70a7ddc6
Eric Hanko <v-erhank@microsoft.com>
2017-08-11 01:02:21 -0700

    Better names; remove unncessary functions

--- inqry/form_instructions.py
+++ inqry/form_instructions.py
@@ -19,47 +19,34 @@ class BarcodeData:
 
 
 class FormInstructions(SystemSpecs, BarcodeData):
-    def __init__(self, form_factor=None, user=None):
+    def __init__(self, form_type=None, user=None):
         super().__init__()
-        self.form_factor = form_factor or 'Desktop'
+        self.form_type = form_type or 'Desktop'
         self.user = user or ''
-        self.fieldset_table = {
+        self.form_types = {
             'Desktop': [self.processor, self.memory, self.drive1, self.drive2, self.drive3, self.drive4],
             'Portable': [self.processor, self.memory, self.drive1],  # TODO: Use enumerate() for storage
-            'Mobile': [self.imei, self.mobile_storage],
-            'New Model': [self.model_name, self.model_identifier]}
+            'Mobile': [self.imei, self.mobile_storage]}
 
     @property
     def processor(self) -> str:
         return '{} {}'.format(self.processor_speed, self.processor_name)
 
-    def instruction_steps(self) -> list:
-        asset_fieldset = self.create_asset_fieldset()
-        user_fieldset = self.user_sequence_fields()
-        return self.basic_fields(asset_fieldset, user_fieldset)
+    def get_form_type_data(self) -> list:
+        return self.form_types[self.form_type]
 
-    def create_asset_fieldset(self) -> list:
-        try:
-            asset_fieldset = self.fieldset_table[self.form_factor]
-        except KeyError:
-            asset_fieldset = self.fieldset_table['New Model']
-        return self.boxify_components(asset_fieldset)
+    def get_asset_sequence(self) -> str:
+        asset_data = self.get_form_type_data()
+        return ''.join([self.textify(component) for component in asset_data])
 
-    def basic_fields(self, unique_fields: list, user_sequence: list, status=None) -> list:
-        status = status or 'Ready'
-        return self.listify(self.model_identifier) + unique_fields + \
-               self.listify(status) + user_sequence + self.delayify(self.serial_number)
-
-    def boxify_components(self, *args) -> list:
-        """
-        Utilizes the textify() to return a list containing appropriate data to scan data into
-        a text box and wrap that data around each of the given components
-        """
-        return [self.textify(component) for component in args]
-
-    def new_model_fields(self, model: str, identifier: str) -> list:
-        return [self.textify(model), self.tab * 2, self.delay, identifier]
-
-    def user_sequence_fields(self) -> list:
+    def get_user_sequence(self) -> str:
         user_sequence = [self.space, self.user, self.enter, self.tab]
-        return [self.delayify(char) for char in user_sequence]
+        return ''.join([self.delayify(char) for char in user_sequence])
+
+    def new_asset(self, status=None) -> str:
+        status = status or 'Ready'
+        return self.listify(self.model_identifier) + self.get_asset_sequence() + self.listify(
+            status) + self.get_user_sequence() + self.delayify(self.serial_number)
+
+    def new_model(self) -> str:
+        return self.textify(self.model_name) + self.tab * 2 + self.delayify(self.model_identifier)
