9f672709ffaa11133aeb183693a4930772cea3fb
Eric Hanko <v-erhank@microsoft.com>
2017-08-10 22:47:40 -0700

    Refactor FormInstructions; abstract BarcodeData class

--- inqry/form_instructions.py
+++ inqry/form_instructions.py
@@ -1,62 +1,61 @@
 from inqry.system_specs.systemspecs import SystemSpecs
 
 
-class FormInstructions(SystemSpecs):
-    def __init__(self, form_factor=None, user=None):
-        super().__init__()
-        self.processor = self.format_processor()
-        self.short_delay = '~d'
-        self.long_delay = '~d~d'
+class BarcodeData:
+    def __init__(self):
+        self.delay = '~d'
         self.tab = '~t'
         self.enter = '~e'
         self.space = '\x20'
+
+    def textify(self, value: str) -> list:
+        return [self.delay, value, self.tab]
+
+    def listify(self, value: str) -> list:
+        return [self.delayify(item) for item in [self.space, value, self.enter, self.tab]]
+
+    def delayify(self, value: str, amount=1) -> str:
+        delay_count = self.delay * amount
+        return delay_count + value
+
+
+class FormInstructions(SystemSpecs, BarcodeData):
+    def __init__(self, form_factor=None, user=None):
+        super().__init__()
         self.status = 'Ready'
         self.form_factor = form_factor or 'Desktop'
-        self.user = user or ''
-        self.fieldsets = self._create_fieldset_table()
+        self.user_sequence = [self.space, user, self.enter, self.tab]
+        self.fieldset_table = {
+            'Desktop': [self.processor, self.memory, self.drive1, self.drive2, self.drive3, self.drive4],
+            'Portable': [self.processor, self.memory, self.drive1],  # TODO: Use enumerate() for storage
+            'Mobile': [self.imei, self.mobile_storage],
+            'New Model': [self.model_name, self.model_identifier]}
 
-    def instruction_steps(self) -> list:
-        if self.form_factor == 'New Model':
-            return self.fieldsets['New Model']
-        else:
-            fieldset = self.fieldsets[self.form_factor]
-            return self._common_fields(fieldset)
-
-    def format_processor(self) -> str:
+    @property
+    def processor(self) -> str:
         return '{} {}'.format(self.processor_speed, self.processor_name)
 
-    def _common_fields(self, unique_fields) -> list:
-        user_sequence = [self.short_delay, self.space,
-                         self.short_delay, self.user,
-                         self.short_delay, self.enter,
-                         self.short_delay, self.tab]
-        return (self._list_box(self.model_identifier) + unique_fields + self._list_box(self.status) + user_sequence + [
-            self.short_delay, self.serial_number])
+    def instruction_steps(self) -> list:
+        asset_fieldset = self.create_asset_fieldset()
+        user_fieldset = self.user_sequence_fields()
+        return self.basic_fields(asset_fieldset, user_fieldset)
 
-    def _text_box(self, field_content) -> list:
-        return [self.short_delay, field_content, self.tab]
+    def create_asset_fieldset(self):
+        try:
+            asset_fieldset = self.fieldset_table[self.form_factor]
+        except KeyError:
+            asset_fieldset = self.fieldset_table['New Model']
+        return self.add_text_to_components(asset_fieldset)
 
-    def _list_box(self, field_content) -> list:
-        return [self.space, self.short_delay, field_content, self.long_delay, self.enter, self.long_delay, self.tab,
-                self.short_delay]
+    def basic_fields(self, unique_fields: list, user_sequence: list) -> list:
+        return self.listify(self.model_identifier) + unique_fields + \
+               self.listify(self.status) + user_sequence + self.delayify(self.serial_number)
 
-    def _create_fieldset_table(self) -> dict:
-        return {'Desktop': (self._text_box(self.processor) +
-                            self._text_box(self.memory) +
-                            self._text_box(self.drive1) +
-                            self._text_box(self.drive2) +
-                            self._text_box(self.drive3) +
-                            self._text_box(self.drive4)),
+    def add_text_to_components(self, *args) -> list:
+        return [self.textify(component) for component in args]
 
-                'Portable': (self._text_box(self.processor) +
-                             self._text_box(self.memory) +
-                             self._text_box(self.drive1)),
+    def new_model_fields(self, model: str, identifier: str):
+        return [self.textify(model), self.tab * 2, self.delay, identifier]
 
-                'New Model': (self._text_box(self.model_name) +
-                              [self.tab,
-                               self.tab,
-                               self.short_delay +
-                               self.model_identifier]),
-
-                'Mobile': (self._text_box(self.imei) +
-                           self._text_box(self.mobile_storage))}
+    def user_sequence_fields(self):
+        return [self.delayify(char) for char in self.user_sequence]
