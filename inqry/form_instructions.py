123a0583b0fc96758331ad60eb353870143fc78d
Jacob Zaval <jazava@microsoft.com>
2017-08-23 22:36:36 -0700

    remove commented Portable

--- inqry/form_instructions.py
+++ inqry/form_instructions.py
@@ -8,10 +8,7 @@ class FormInstructions:
         self.processor = '{} {}'.format(specs.processor_speed, specs.processor_name)
         self.form_types = {'Desktop': [self.processor, specs.memory,
                                        specs.drive1, specs.drive2,
-                                       specs.drive3, specs.drive4],
-                        #    'Portable': [self.processor, specs.memory,
-                        #                 specs.drive1]
-                            }
+                                       specs.drive3, specs.drive4],}
         self.model_identifier = specs.model_identifier
         self.model_name = specs.model_name
         self.serial_number = specs.serial_number
