a3d415c2168eecb2b75aa5de6176d43b3ad26f31
Andre Shields <v-anshie@microsoft.com>
2017-08-31 10:36:47 -0700

    removal of Portable form_type in form_instructions.py

--- inqry/form_instructions.py
+++ inqry/form_instructions.py
@@ -8,9 +8,7 @@ class FormInstructions:
         self.processor = '{} {}'.format(specs.processor_speed, specs.processor_name)
         self.form_types = {'Desktop': [self.processor, specs.memory,
                                        specs.drive1, specs.drive2,
-                                       specs.drive3, specs.drive4],
-                           'Portable': [self.processor, specs.memory,
-                                        specs.drive1]
+                                       specs.drive3, specs.drive4]                
                             }
         self.model_identifier = specs.model_identifier
         self.model_name = specs.model_name
